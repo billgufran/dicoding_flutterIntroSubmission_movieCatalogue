567c37258119f8eb130dbf67217cabbbbba6f528
Fikri Bill Gufran <billgufran@gmail.com>
2022-08-05 20:00:22 +0800

    make movie model

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -1,33 +1,43 @@
 class MovieModel {
+  String backdropPath;
+  String firstAirDate;
+  List<int> genreIds;
+  int id;
   String name;
-  String location;
-  String description;
-  String openDays;
-  String openTime;
-  String ticketPrice;
-  String imageAsset;
-  List<String> imageUrls;
+  List<String> originCountry;
+  String originalLanguage;
+  int popularity;
+  String posterPath;
+  int voteAverage;
+  int voteCount;
 
-  MovieModel(
-      {required this.name,
-      required this.location,
-      required this.description,
-      required this.openDays,
-      required this.openTime,
-      required this.ticketPrice,
-      required this.imageAsset,
-      required this.imageUrls});
+  MovieModel({
+    required this.backdropPath,
+    required this.firstAirDate,
+    required this.genreIds,
+    required this.id,
+    required this.name,
+    required this.originCountry,
+    required this.originalLanguage,
+    required this.popularity,
+    required this.posterPath,
+    required this.voteAverage,
+    required this.voteCount,
+  });
 
-
-   factory MovieModel.fromJson(Map<String, dynamic> json) {
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
     return MovieModel(
       backdropPath: json['backdrop_path'],
       firstAirDate: json['first_air_date'],
       genreIds: json['genre_ids'],
-      title: json['genre_ids'],
-      title: json['genre_ids'],
-      title: json['genre_ids'],
+      id: json['id'],
+      name: json['name'],
+      originCountry: json['origin_country'],
+      originalLanguage: json['original_language'],
+      popularity: json['popularity'],
+      posterPath: json['poster_path'],
+      voteAverage: json['vote_average'],
+      voteCount: json['vote_count'],
     );
   }
 }
-
