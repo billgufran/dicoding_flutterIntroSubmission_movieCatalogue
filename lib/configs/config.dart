40afd2dbd9dbdf7b0aaa73534295538f2d8fa7d3
Fikri Bill Gufran <billgufran@gmail.com>
2022-08-08 19:20:19 +0800

    remove image config from ImageHost

--- lib/configs/config.dart
+++ lib/configs/config.dart
@@ -1,5 +1,5 @@
 class Config {
   static String mdbApiHost = "https://api.themoviedb.org/3";
   static String mdbApiKey = "7df4b9bc13d27dee49d5e5411620fdfd";
-  static String mdbImageHost = "https://image.tmdb.org/t/p/original";
+  static String mdbImageHost = "https://image.tmdb.org/t/p";
 }
