8c037335c4ec38f45d1349be666ec53463d83c40
Fikri Bill Gufran <billgufran@gmail.com>
2022-08-09 23:32:59 +0800

    create desktop view for detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moviecatalogue/Widgets/custom_app_bar.dart';
 import 'package:moviecatalogue/models/movie_model.dart';
 import 'package:moviecatalogue/configs/config.dart';
 import 'package:moviecatalogue/theme/colors.dart';
@@ -7,6 +8,27 @@ class DetailScreen extends StatelessWidget {
   final MovieModel movie;
 
   const DetailScreen({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            return MobileView(movie: movie);
+          } else {
+            return DesktopView(movie: movie);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class MobileView extends StatelessWidget {
+  final MovieModel movie;
+
+  const MobileView({Key? key, required this.movie}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -66,6 +88,109 @@ class DetailScreen extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class DesktopView extends StatelessWidget {
+  final MovieModel movie;
+
+  const DesktopView({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          // transform: Matrix4.translationValues(0.0, -60.0, 0.0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                '${Config.mdbImageHost}/original${movie.backdropPath}',
+              ),
+              colorFilter: ColorFilter.mode(
+                CustomColors.backgroundColor.withOpacity(0.5),
+                BlendMode.dstATop,
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 28.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 28.0),
+            child: Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        '${Config.mdbImageHost}/w342${movie.posterPath}',
+                        fit: BoxFit.cover,
+                        width: 300,
+                        height: 300 * 1.5,
+                      ),
+                    ),
+                    const SizedBox(width: 38.0),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                movie.title!,
+                                style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  color: CustomColors.textColor,
+                                ),
+                              ),
+                              const SizedBox(width: 8.0),
+                              Text(
+                                '(${movie.releaseDate!.split('-')[0]})',
+                                style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.w200,
+                                  color: CustomColors.textColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 25),
+                          const FloatingFavoriteButton(),
+                          const SizedBox(height: 25),
+                          Text(
+                            'Overview',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: CustomColors.textColor,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            movie.overview!,
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
