f83b04c824cce770117a20a5f3dd1f4bc6860059
Fikri Bill Gufran <billgufran@gmail.com>
2022-08-09 11:45:06 +0800

    change endpoint to movie now playing and top rated

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,8 +1,8 @@
 import 'package:moviecatalogue/configs/config.dart';
 
 class MdbEndpoints {
-  static String movieDiscoverPath =
-      '/discover/movie?api_key=${Config.mdbApiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
-  static String tvDiscoverPath =
-      '/discover/tv?api_key=${Config.mdbApiKey}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0';
+  static String movieNowPlayingPath =
+      '/movie/now_playing?api_key=${Config.mdbApiKey}&language=en-US&page=1';
+  static String movieTopRatedPath =
+      '/movie/top_rated?api_key=${Config.mdbApiKey}&language=en-US&page=1';
 }
--- lib/data/datasource.dart
+++ lib/data/datasource.dart
@@ -4,21 +4,32 @@ import 'package:moviecatalogue/configs/config.dart';
 import 'package:moviecatalogue/data/constants.dart';
 import 'package:moviecatalogue/models/movie_model.dart';
 
-Future<List<MovieModel>> fetchMovieDiscovery() async {
-  final response = await http
-      .get(Uri.parse('${Config.mdbApiHost}${MdbEndpoints.movieDiscoverPath}'));
+Future<List<MovieModel>> fetchMovieNowPlaying() async {
+  final response = await http.get(
+      Uri.parse('${Config.mdbApiHost}${MdbEndpoints.movieNowPlayingPath}'));
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
     final responseResults = jsonDecode(response.body)['results'];
-    // print(responseResults['results']);
+
     return responseResults
         .map<MovieModel>((json) => MovieModel.fromJson(json))
         .toList();
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
+    throw Exception('Failed to load movies');
+  }
+}
+
+Future<List<MovieModel>> fetchMovieTopRated() async {
+  final response = await http
+      .get(Uri.parse('${Config.mdbApiHost}${MdbEndpoints.movieTopRatedPath}'));
+
+  if (response.statusCode == 200) {
+    final responseResults = jsonDecode(response.body)['results'];
+
+    return responseResults
+        .map<MovieModel>((json) => MovieModel.fromJson(json))
+        .toList();
+  } else {
     throw Exception('Failed to load movies');
   }
 }
