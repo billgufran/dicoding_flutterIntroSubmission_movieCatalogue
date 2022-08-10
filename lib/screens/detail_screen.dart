c31c06d7fe9a25e4ec6732d94aed81c1e22ac595
Fikri Bill Gufran <billgufran@gmail.com>
2022-08-10 22:22:42 +0800

    adjust scroll and image fit

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -34,61 +34,59 @@ class MobileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: const FloatingFavoriteButton(),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.4,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                    '${Config.mdbImageHost}/w780${movie.backdropPath}',
-                  ),
-                  fit: BoxFit.cover,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  '${Config.mdbImageHost}/w780${movie.backdropPath}',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: ShapeDecoration(
+              color: CustomColors.backgroundColor,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16.0),
+                  topRight: Radius.circular(16.0),
                 ),
               ),
             ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: ShapeDecoration(
-                color: CustomColors.backgroundColor,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16.0),
-                    topRight: Radius.circular(16.0),
+            transform: Matrix4.translationValues(0.0, -50.0, 0.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 28.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  movie.title!,
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  movie.releaseDate!,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                    color: CustomColors.textColor,
                   ),
                 ),
-              ),
-              transform: Matrix4.translationValues(0.0, -50.0, 0.0),
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 28.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    movie.title!,
-                    style: Theme.of(context).textTheme.headline2,
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    movie.releaseDate!,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w300,
-                      color: CustomColors.textColor,
-                    ),
-                  ),
-                  const SizedBox(height: 32),
-                  Text(
-                    movie.overview!,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                ],
-              ),
+                const SizedBox(height: 32),
+                Text(
+                  movie.overview!,
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -101,46 +99,50 @@ class DesktopView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double appBarHeight = 60.0;
+
     return Scaffold(
-      appBar: const CustomAppBar(),
+      appBar: const CustomAppBar(height: appBarHeight),
       extendBodyBehindAppBar: true,
-      body: SingleChildScrollView(
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 28.0),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              '${Config.mdbImageHost}/original${movie.backdropPath}',
+            ),
+            colorFilter: ColorFilter.mode(
+              CustomColors.backgroundColor.withOpacity(0.5),
+              BlendMode.dstATop,
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Container(
           width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                '${Config.mdbImageHost}/original${movie.backdropPath}',
-              ),
-              colorFilter: ColorFilter.mode(
-                CustomColors.backgroundColor.withOpacity(0.5),
-                BlendMode.dstATop,
-              ),
-              fit: BoxFit.cover,
-            ),
-          ),
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 28.0),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 28.0),
-            child: Center(
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.network(
-                        '${Config.mdbImageHost}/w342${movie.posterPath}',
-                        fit: BoxFit.cover,
-                        width: 300,
-                        height: 300 * 1.5,
-                      ),
+          padding:
+              const EdgeInsets.fromLTRB(16.0, appBarHeight + 18.0, 16.0, 18.0),
+          child: Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      '${Config.mdbImageHost}/w342${movie.posterPath}',
+                      fit: BoxFit.contain,
+                      alignment: Alignment.center,
+                      width: 300,
+                      height: 300 * 1.5,
                     ),
-                    const SizedBox(width: 38.0),
-                    Expanded(
+                  ),
+                  const SizedBox(width: 38.0),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -186,8 +188,8 @@ class DesktopView extends StatelessWidget {
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
